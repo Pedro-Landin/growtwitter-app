2f49f3841ed976be77814507121137d818723c53
Pedro henrique landin <henriquep35036@gmail.com>
2026-04-17 15:33:09 -0300

    feat: proteção de rotas para caso o usuario não esteja logado

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,7 +1,21 @@
-import { Outlet } from "react-router";
+import { Outlet, Navigate } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 import Header from "../components/Header";
 
 export function DefaultLayout() {
+    const { isAuthenticated, loading } = useAuth();
+
+    // 1. Enquanto o App checa o localStorage, mostramos um loading
+    if (loading) {
+        return <h1>Carregando...</h1>;
+    }
+
+    // 2. Se não estiver autenticado, redireciona para o login
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
+
+    // 3. Se estiver logado, renderiza o layout completo
     return (
         <>
             <Header />
@@ -9,6 +23,5 @@ export function DefaultLayout() {
             <Outlet />
             <h1>Right</h1>
         </>
-
     )
 }
