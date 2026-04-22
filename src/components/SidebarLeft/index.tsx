e915af9fa74a0825647dbf8065f588c3cea56250
Pedro henrique landin <henriquep35036@gmail.com>
2026-04-22 20:51:54 -0300

    feat: Criação do menu lateral: Layout + lógica de menu

--- src/components/SidebarLeft/index.tsx
+++ src/components/SidebarLeft/index.tsx
@@ -1,3 +1,79 @@
+import { useAuth } from "../../hooks/useAuth";
+import { useTheme } from "../../hooks/useTheme";
+import logo from '../../assets/logo.png'
+import icon from '../../assets/icon.png'
+
+// Icons Explorar
+import icon_Explorar_Dark from '../../assets/icon_Explorar_Dark.png'
+import icon_Explorar_Light from '../../assets/icon_Explorar_Light.png'
+// Icons Pagina Inicial
+import icon_Paginainicial_Dark from '../../assets/icon_Paginainicial_Dark.png'
+import icon_Paginainicial_Light from '../../assets/icon_Paginainicial_Light.png'
+// Icons Perfil
+import icon_Perfil_Dark from '../../assets/icon_Perfil_Dark.png'
+import icon_Perfil_Light from '../../assets/icon_Perfil_Light.png'
+
+import { BlockMenu, ContainerSidebarLeft, LinkMenu, ButtonTweet, CardUser, CardUserInfo } from "./styles";
+
 export default function SidebarLeft() {
-    return <div>SidebarLeft</div>;
+    const { user } = useAuth();
+    const { theme } = useTheme();
+
+    return (
+        <ContainerSidebarLeft>
+            <div className="logo">
+                <img src={logo} alt="Growtwitter" />
+            </div>
+
+            <BlockMenu>
+                <LinkMenu to="/" end>
+                    {
+                        theme === 'dark' ? (
+                            <img src={icon_Paginainicial_Dark} alt="Página Inicial" />
+                        ) : (
+                            <img src={icon_Paginainicial_Light} alt="Página Inicial" />
+                        )
+                    }
+                    <span>Página Inicial</span>
+                </LinkMenu>
+
+                <LinkMenu to="/explore">
+                    {
+                        theme === 'dark' ? (
+                            <img src={icon_Explorar_Dark} alt="Explorar" />
+                        ) : (
+                            <img src={icon_Explorar_Light} alt="Explorar" />
+                        )
+                    }
+                    <span>Explorar</span>
+                </LinkMenu>
+
+                <LinkMenu to="/profile">
+                    {
+                        theme === 'dark' ? (
+                            <img src={icon_Perfil_Dark} alt="Perfil" />
+                        ) : (
+                            <img src={icon_Perfil_Light} alt="Perfil" />
+                        )
+                    }
+                    <span>Perfil</span>
+                </LinkMenu>
+            </BlockMenu>
+
+            <ButtonTweet>Tweetar</ButtonTweet>
+
+            {/* Card do Usuário Logado */}
+            <CardUser>
+                <div className="avatar">
+                    <div style={{ width: 40, height: 40, borderRadius: '50%', background: '#E0E0E0', display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
+                        <img src={icon} alt="Avatar" />
+                    </div>
+                </div>
+                <CardUserInfo>
+                    <strong>{user?.name}</strong>
+                    <span>@{user?.username}</span>
+                </CardUserInfo>
+            </CardUser>
+        </ContainerSidebarLeft>
+    );
 }
---
+++ src/components/SidebarLeft/styles.ts
@@ -0,0 +1,67 @@
+import { NavLink } from "react-router";
+import styled from 'styled-components';
+
+export const ContainerSidebarLeft = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    gap: 15px;
+    align-items: flex-start;
+`;
+
+export const BlockMenu = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+
+export const LinkMenu = styled(NavLink)`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    align-items: center;
+    text-decoration: none;
+    color: ${props => props.theme.colors.textColor};
+    font-size: 14px;
+    font-weight: 400;
+    cursor: pointer;
+
+    &.active {
+        font-weight: 800;
+    }
+`;
+
+export const ButtonTweet = styled.button`
+    background-color: ${props => props.theme.colors.primary};
+    color: #fff;
+    cursor: pointer;
+    padding: 7px 15px;
+    border-radius: 15px;
+    font-size: 14px;
+    width: 100%;
+`;
+
+export const CardUser = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    align-items: center;
+`;
+
+export const CardUserInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    strong {
+        color: ${props => props.theme.colors.textColor};
+        font-weight: 800;
+        font-size: 12px;
+    }
+
+    span {
+        color: #828282;
+        font-weight: 400;
+        font-size: 12px;
+    }
+`;
